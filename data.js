22274fd4f63147c57bd999a3481e6e0d4e607e1d
OmegaSkiller <boss.ivailo@gmail.com>
2025-03-21 13:49:13 +0200

    Initial setup of project files and directory structure.

---
+++ data.js
@@ -0,0 +1,29 @@
+const participants = [
+    {
+        id: "1",
+        firstName: "Вячеслав",
+        lastName: "Шевченко",
+        company: "Home Electronics Group (HEG)",
+        fullName: "Вячеслав Шевченко",
+        image: "assets/faces/1.jpg",
+        presentation: "assets/slides/1.jpg"
+    },
+    {
+        id: "2",
+        firstName: "Василка",
+        lastName: "Горанова-Харбова",
+        company: "Leonidas",
+        fullName: "Василка Горанова-Харбова",
+        image: "assets/faces/2.jpg",
+        presentation: "assets/slides/2.jpg"
+    },
+    {
+        id: "3",
+        firstName: "Иван",
+        lastName: "Парашкевов",
+        company: "Пасажа",
+        fullName: "Иван Парашкевов",
+        image: "assets/faces/3.jpg",
+        presentation: "assets/slides/3.jpg"
+    }
+]; 
